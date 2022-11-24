dfe312e4dc2de736cc3f324f390ab40705b9f259
Jean <jeanncavalcante@gmail.com>
2022-11-23 22:42:22 -0300

    feat: check

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components";
 
 import Main from "./src/screen/Main";
 import theme from "./src/Theme";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
-    <>
+    <NativeBaseProvider>
       <ThemeProvider theme={theme}>
         <StatusBar style="light" />
         <Main />
       </ThemeProvider>
-    </>
+    </NativeBaseProvider>
   );
 }
